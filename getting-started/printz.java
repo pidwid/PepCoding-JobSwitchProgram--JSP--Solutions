f9da98c3af39725947c11b5e2b6caf66c38a2fb4
pidwid <luckyshukla765@gmail.com>
2021-06-16 08:08:04 +0530

    initial commit

---
+++ getting-started/printz.java
@@ -0,0 +1,14 @@
+// Print Z
+// https://classroom.pepcoding.com/myClassroom/the-switch-program-3/getting-started/print-z-official/ojquestion
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("*****");
+        System.out.println("   * ");
+        System.out.println("  *  ");
+        System.out.println(" *   ");
+        System.out.println("*****");
+
+    }
+}
