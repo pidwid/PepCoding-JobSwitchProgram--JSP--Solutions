df301cf7f72db9329c8ee24da4e97994f89a1972
pidwid <luckyshukla765@gmail.com>
2021-06-23 13:09:45 +0530

    backtracking

--- recursion-backtracking/n-queens.java
+++ recursion-backtracking/n-queens.java
@@ -1,3 +1,39 @@
-public class n-queens {
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+        printNQueens(arr, "", 0);
+    }
+
+    public static void printNQueens(int[][] chess, String qsf, int row) {
+        if(row == chess.length){
+            System.out.println(qsf + ".");
+            return;
+        }
+        for(int col = 0; col < chess.length; col++){
+            if(placeIsSafe(chess, row, col) == true){
+                chess[row][col] = 1;
+                printNQueens(chess, qsf + row + "-" + col + ", ", row + 1);
+                chess[row][col] = 0;
+            }   
+        }
+    }
     
+    public static boolean placeIsSafe(int[][] chess, int row, int col){
+        for(int i = row - 1, j = col; i >= 0; i--){
+            if(chess[i][j] == 1) return false;
+        }
+        for(int i = row - 1, j = col - 1; i >= 0 && j>= 0; i--, j--){
+            if(chess[i][j] == 1) return false;
+        }
+        for(int i = row - 1, j = col + 1; i >= 0 && j < chess.length; i--, j++){
+            if(chess[i][j] == 1) return false;
+        }
+        return true;
+    }
 }
--- recursion-backtracking/target-sum-subsets.java
+++ recursion-backtracking/target-sum-subsets.java
@@ -1,3 +1,33 @@
-public class target-sum-subsets {
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int tar = sc.nextInt();
+        printTargetSumSubsets(arr, 0, "", 0, tar);
+    }
+
+    // set is the subset
+    // sos is sum of subset
+    // tar is target
+    public static void printTargetSumSubsets(int[] arr,int idx, String set, int sos, int tar) {
+        if(idx == arr.length){
+            if(sos == tar){
+                System.out.println(set + ".");
+                return;
+            }else return;
+        }
+        printTargetSumSubsets(arr, idx + 1, set + arr[idx] + ", ", sos + arr[idx], tar);
+        printTargetSumSubsets(arr, idx + 1, set, sos, tar);
+    }
     
 }
