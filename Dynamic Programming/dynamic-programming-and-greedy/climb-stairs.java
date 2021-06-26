98f493aad2c67eefa5a4c9dc41d22db023614f3b
pidwid <luckyshukla765@gmail.com>
2021-06-26 21:29:12 +0530

    dp

--- Dynamic Programming/dynamic-programming-and-greedy/climb-stairs.java
+++ Dynamic Programming/dynamic-programming-and-greedy/climb-stairs.java
@@ -1,1 +1,45 @@
-// 
+// Climb Stairs
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(getCount(n, new int[n+1]));
+        System.out.println(getCountTab(n));
+    }
+    
+    public static int getCount(int n, int[] x){
+        if(n == 0) return 1;
+        if(n < 0) return 0;
+        if(x[n] > 0) return x[n];
+        int count = 0;
+        int n1 = getCount(n - 1, x);
+        int n2 = getCount(n - 2, x);
+        int n3 = getCount(n - 3, x);
+        count += n1 + n2 + n3;
+        x[n] = count;
+        return count;
+    }
+    
+    
+    public static int getCountTab(int n){
+        
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        for(int i = 1; i <= n; i++){
+            if(i == 1){
+                dp[i] = dp[i - 1];                
+            }else if(i == 2){
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }else{
+                dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+            }
+        }
+        return dp[n];
+    }
+    
+
+}
--- Dynamic Programming/dynamic-programming-and-greedy/fibonacci-dp.java
+++ Dynamic Programming/dynamic-programming-and-greedy/fibonacci-dp.java
@@ -1,1 +1,23 @@
-// 
+// Fibonacci DP
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    System.out.print(fib(n, new HashMap<>() ));
+    
+ }
+ 
+ public static int fib(int n, HashMap<Integer, Integer> memo){
+     if(n == 0 || n == 1) return n;
+     if(memo.containsKey(n)) return memo.get(n);
+        int x =  fib(n-1, memo) + fib(n - 2, memo);
+        memo.put(n, x);
+        return x;
+ }
+
+}
