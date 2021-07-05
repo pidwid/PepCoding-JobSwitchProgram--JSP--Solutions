d40f1065719522b3f5472ca2812b81359b1afdc9
pidwid <luckyshukla765@gmail.com>
2021-07-05 21:41:23 +0530

    merge

--- Dynamic_Programming/time-and-space-complexity/merge-two-sorted-arrays.java
+++ Dynamic_Programming/time-and-space-complexity/merge-two-sorted-arrays.java
@@ -1,1 +1,42 @@
-// 
+// Merge two sorted array
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static int[] mergeTwoSortedArrays(int[] a, int[] b){
+      int alen = a.length, blen = b.length;
+     int[] res = new int[alen + blen];
+     int i = 0, j = 0, idx = 0;
+     while(i < alen && j < blen){
+        if(a[i] < b[j]) res[idx++] = a[i++];
+        else res[idx++] = b[j++];
+     }
+     while(j < blen) res[idx++] = b[j++];
+     while(i < alen) res[idx++] = a[i++];
+    
+    return res;
+  }
+
+  public static void print(int[] arr){
+    for(int i = 0 ; i < arr.length; i++){
+      System.out.println(arr[i]);
+    }
+  }
+  public static void main(String[] args){
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] a = new int[n];
+    for(int i = 0 ; i < n; i++){
+      a[i] = scn.nextInt();
+    }
+    int m = scn.nextInt();
+    int[] b = new int[m];
+    for(int i = 0 ; i < m; i++){
+      b[i] = scn.nextInt();
+    }
+    int[] mergedArray = mergeTwoSortedArrays(a,b);
+    print(mergedArray);
+  }
+
+}
--- Dynamic_Programming/time-and-space-complexity/selection-sort.java
+++ Dynamic_Programming/time-and-space-complexity/selection-sort.java
@@ -1,1 +1,55 @@
-// 
+// Selection Sort
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void selectionSort(int[] arr) {
+     int len = arr.length;
+    int min = 0;
+    for(int itr = 0; itr < len - 1; itr++){
+        min = itr;
+        for(int i = itr +1 ; i < len; i++){
+            if(isSmaller(arr,i, min)) min = i;
+        }
+        swap(arr, itr, min);
+    }
+
+  }
+
+  // used for swapping ith and jth elements of array
+  public static void swap(int[] arr, int i, int j) {
+    System.out.println("Swapping " + arr[i] + " and " + arr[j]);
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  // return true if ith element is smaller than jth element
+  public static boolean isSmaller(int[] arr, int i, int j) {
+    System.out.println("Comparing " + arr[i] + " and " + arr[j]);
+    if (arr[i] < arr[j]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public static void print(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = scn.nextInt();
+    }
+    selectionSort(arr);
+    print(arr);
+  }
+
+}
