bc22da78d1b75b4f06f62859845e2d50c69481ab
pidwid <luckyshukla765@gmail.com>
2021-07-06 10:21:15 +0530

    quick

--- Dynamic_Programming/time-and-space-complexity/partition-an-array.java
+++ Dynamic_Programming/time-and-space-complexity/partition-an-array.java
@@ -1,1 +1,59 @@
-// 
+// Partition an array
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void partition(int[] arr, int pivot){
+    int i = 0, j = 0;
+    while(i < arr.length){
+        if(arr[i] <= pivot){
+            swap(arr, i, j);
+            j++;
+            i++;
+        }else i++;
+    }
+    
+  }
+  
+  /** MY SOLution
+   public static void partition(int[] arr, int pivot){
+    int i = 0, j = arr.length-1;
+    while(i <= j){
+        if(arr[i] > pivot){
+            swap(arr, i, j);
+            j--;
+        }else i++;
+    }
+    
+  }
+   */ 
+
+  // used for swapping ith and jth elements of array
+  public static void swap(int[] arr, int i, int j) {
+    System.out.println("Swapping " + arr[i] + " and " + arr[j]);
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  public static void print(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+  }
+
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = scn.nextInt();
+    }
+    int pivot = scn.nextInt();
+    partition(arr,pivot);
+    print(arr);
+  }
+
+}
