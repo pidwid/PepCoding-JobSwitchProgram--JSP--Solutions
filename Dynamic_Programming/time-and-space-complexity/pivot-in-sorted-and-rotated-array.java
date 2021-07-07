d8a2de040d4ff23518cec1c90e6c01d24dbb1abb
pidwid <luckyshukla765@gmail.com>
2021-07-07 23:36:06 +0530

    t&C

--- Dynamic_Programming/time-and-space-complexity/pivot-in-sorted-and-rotated-array.java
+++ Dynamic_Programming/time-and-space-complexity/pivot-in-sorted-and-rotated-array.java
@@ -1,1 +1,29 @@
-// 
+// Pivot In Sorted And Rotated Array
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static int findPivot(int[] arr) {
+    int lo = 0, hi = arr.length - 1;
+    while(lo < hi){
+        int mid = (lo + hi) / 2;
+        if(arr[mid] < arr[hi]) hi = mid;
+        else lo = mid + 1;
+    }
+    return arr[lo];
+  }
+
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = scn.nextInt();
+    }
+    int pivot = findPivot(arr);
+    System.out.println(pivot);
+  }
+
+}
--- Dynamic_Programming/time-and-space-complexity/target-sum-pair-1.java
+++ Dynamic_Programming/time-and-space-complexity/target-sum-pair-1.java
@@ -1,1 +1,34 @@
-// 
+// Target sum pair
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void targetSumPair(int[] arr, int target){
+        //write your code here
+        Arrays.sort(arr); // T(nlogn) sort
+        int l = 0, r = arr.length - 1;
+        while(l < r){
+            int small = arr[l], big = arr[r];
+            if(small + big > target) r--;
+            else if(small + big < target) l++;
+            else{
+                System.out.println(small + ", "+ big);
+                l++;
+                r--;
+            }
+        }
+
+  }
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for(int i = 0 ;i < n; i++){
+      arr[i] = scn.nextInt();
+    }
+    int target = scn.nextInt();
+    targetSumPair(arr,target);
+  }
+
+}
