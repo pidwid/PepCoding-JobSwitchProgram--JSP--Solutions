f1f6de041b566f9f66c60788bf9b183593dd9ecc
pidwid <luckyshukla765@gmail.com>
2021-07-10 18:59:58 +0530

    dp

--- Dynamic_Programming/dynamic-programming-and-greedy/climb-stairs-with-minimum-moves.java
+++ Dynamic_Programming/dynamic-programming-and-greedy/climb-stairs-with-minimum-moves.java
@@ -1,32 +1,85 @@
-// Climb Stairs With Minimum Moves
 import java.io.*;
 import java.util.*;
+import java.lang.*;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-         Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
         for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
         }
-        System.out.println(minClimb(n, arr));
+        //System.out.println(minClimb(0, arr, new int[n+1]));
+        //System.out.println(minClimbTab(n, arr));
+        //System.out.println(minClimbTabTop(n, arr));
+        System.out.println(minClimbOpt(n, arr));
+    }
+    // Not working
+    public static int minClimb(int idx, int[] arr, int[] dp){
+        if(idx >= arr.length) return 0;
+        if(idx == arr.length) return 1;
+        if(dp[idx] != 0) return dp[idx];
+        int min = Integer.MAX_VALUE;
+        for(int jump = 1;  idx - 1 >= 0 && jump <= arr[idx - 1]; jump++){
+            if(idx + jump <= arr.length) min =  Math.min(minClimb(idx + jump, arr, dp), min);
+        }
+        dp[idx] = min;
+        return min;
     }
     
-    public static int minClimb(int n, int[] arr){
-        Integer[] res = new Integer[n + 1];
-        res[n] = 0;
+    // Bottom Up  
+    public static int minClimbTab(int n, int[] arr){
+        Integer[] dp = new Integer[n + 1];
+        dp[n] = 0;
         for(int i = n-1; i >= 0; i--){
             int min = Integer.MAX_VALUE;
             for(int j = 1; j<= arr[i]; j++){
-                if(i + j <= n){
-                    if(res[i + j] < min) min = res[i + j] + 1;
+                if(i + j <= n) if(dp[i + j] < min) min = dp[i + j] + 1;
+            }
+            dp[i] = min;
+        }
+        return dp[0];
+    }
+    
+    // Top Down
+    public static int minClimbTabTop(int n, int[] arr){
+        int[] dp = new int[n+1];
+        for(int i = 1; i <= n; i++){ 
+            int min = Integer.MAX_VALUE;
+            for(int j = i-1; j >= 0; j--){
+                if(arr[j] >= i - j){
+                    if(dp[j] < min) min = dp[j];
                 }
             }
-            res[i] = min;
+            dp[i] = min + 1;
         }
-        return res[0];
+        return dp[n];
     }
+    
+    // Optimized  O(n) soln
+    public static int minClimbOpt(int n, int[] arr){
+        int steps = 0;
+        for(int i = 0; i < n;){
+            if(arr[i] == 0) break;
+            int max = 0, maxIdx = 0;
+            for(int jump = 1; jump <= arr[i]; jump++){
+                int j = i + jump;
+                if(j == n - 1){
+                    steps++;
+                    return steps;
+                }
+                if(j + arr[j] >= max){
+                    max = j + arr[j];
+                    maxIdx = j;
+                }
+            }
+            i = maxIdx;
+            steps++;
+        }
+        return Integer.MAX_VALUE;
+    }
+
 
 }
--- Dynamic_Programming/dynamic-programming-and-greedy/min-cost-in-maze-traversal.java
+++ Dynamic_Programming/dynamic-programming-and-greedy/min-cost-in-maze-traversal.java
@@ -1,29 +1,48 @@
+
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(), m = sc.nextInt();
-        int[][] mat = new int[n][m];
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                mat[i][j] = sc.nextInt();
-            }
-        }
-        int[][] res = new int[n][m];
+       Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt(), m = sc.nextInt();
+       int[][] arr = new int[n][m];
+       for(int i = 0; i < n; i++){
+           for(int j = 0; j < m; j++){
+               arr[i][j] = sc.nextInt();
+           }
+       }
+       int[][] dp = new int[n][m];
+       //System.out.print(minCost(arr, 0, 0, dp));
+       System.out.print(minCostTab(arr, n, m, dp));
+    }
+    
+    public static int minCost(int[][] arr, int sr, int sc, int[][] dp){
+        if(sr >= arr.length || sc >= arr[0].length) return Integer.MAX_VALUE;
+        if(sr == arr.length - 1 && sc == arr[0].length - 1) return arr[sr][sc];
+        if(dp[sr][sc] != 0) return dp[sr][sc];
+        int f1 = minCost(arr, sr, sc + 1, dp);
+        int f2 = minCost(arr, sr + 1, sc, dp);
+        int ans = Math.min(f1, f2) + arr[sr][sc];
+        dp[sr][sc] = ans;
+        return ans;
+    }
+    
+    public static int minCostTab(int[][] mat,int n,int m, int[][] dp){
         for(int i = n - 1; i >= 0; i--){
             for(int j = m - 1; j >= 0; j--){
                 int prev = mat[i][j];
-                if(i == n-1 && j == m-1) res[i][j] = prev;
-                else if(i == n - 1) res[i][j] = res[i][j + 1] + prev;
-                else if(j == m - 1) res[i][j] = res[i + 1][j] + prev;
-                else res[i][j] = prev + Math.min(res[i + 1][j], res[i][j + 1]);
+                if(i == n-1 && j == m-1) dp[i][j] = prev;
+                else if(i == n - 1) dp[i][j] = dp[i][j + 1] + prev;
+                else if(j == m - 1) dp[i][j] = dp[i + 1][j] + prev;
+                else dp[i][j] = prev + Math.min(dp[i + 1][j], dp[i][j + 1]);
             }
         }
         
-        System.out.println(res[0][0]);
+        return dp[0][0];
     }
 
 }
+
