90b230897c6836bd21dd985c2b95f5022a04f7c8
pidwid <luckyshukla765@gmail.com>
2021-07-24 20:19:50 +0530

    sliding

--- Basic_Data_Structures/stack-and-queue/largest-area-histogram.java
+++ Basic_Data_Structures/stack-and-queue/largest-area-histogram.java
@@ -1,5 +1,49 @@
-package Basic_Data_Structures.stack-and-queue;
+// Largest Area Histogram
+import java.io.*;
+import java.util.*;
 
-public class largest-area-histogram {
+public class Main{
+  
+
+public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    for(int i = 0; i < n; i++){
+       arr[i] = Integer.parseInt(br.readLine());
+    } 
+
+    Stack<Integer> st = new Stack<>();
+    int[] nsr = new int[n], nsl = new int[n];
+    // nsr
+    for(int i = n-1; i >= 0; i--){
+        // pop all bigger element
+        while(!st.isEmpty() && arr[st.peek()] >= arr[i]) st.pop();
+        // update ans
+        if(st.isEmpty()) nsr[i] = arr.length;
+        else nsr[i] = st.peek();
+        // push current index
+        st.push(i);
+    }
+    // renew stack
+    st = new Stack<>();
     
+    // nsl
+    for(int i = 0; i < n; i++){
+        // pop all bigger element
+        while(!st.isEmpty() && arr[st.peek()] >= arr[i]) st.pop();
+        // update ans
+        if(st.isEmpty()) nsl[i] = -1;
+        else nsl[i] = st.peek();
+        // push current index
+        st.push(i);
+    }
+    // answer
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        ans = Math.max(ans, arr[i] * (nsr[i] - nsl[i] - 1));
+    }
+    System.out.print(ans);  
+ }
 }
--- Basic_Data_Structures/stack-and-queue/sliding-window-maximum.java
+++ Basic_Data_Structures/stack-and-queue/sliding-window-maximum.java
@@ -1,5 +1,36 @@
-package Basic_Data_Structures.stack-and-queue;
+// Sliding Window Maximum
 
-public class sliding-window-maximum {
-    
+import java.io.*;
+import java.util.*;
+
+public class Main{
+  
+
+public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    for(int i = 0; i < n; i++){
+       arr[i] = Integer.parseInt(br.readLine());
+    }
+    int k = Integer.parseInt(br.readLine());
+
+    Stack<Integer> st = new Stack<>();
+    int[] ngr = new int[n];
+    for(int i = n-1; i >= 0; i--){  
+        // pop all smaller
+        while(!st.isEmpty() && arr[st.peek()] < arr[i]) st.pop();
+        if(st.isEmpty()) ngr[i] = arr.length;
+        else ngr[i] = st.peek();
+        // push curr index
+        st.push(i);
+    }
+    for(int i = 0, j = 0; i <= n - k; i++){
+        int window = i+k;
+        if(j < i) j = i;
+        while(ngr[j] < window) j = ngr[j];
+        System.out.println(arr[j]);
+    }
+ }
 }
