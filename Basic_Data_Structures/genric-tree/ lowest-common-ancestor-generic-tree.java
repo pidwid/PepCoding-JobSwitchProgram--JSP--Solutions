f7026b2f5350abd703ea44a88a00637b55604278
pidwid <luckyshukla765@gmail.com>
2021-08-15 01:47:18 +0530

    genric

--- Basic_Data_Structures/genric-tree/ lowest-common-ancestor-generic-tree.java
+++ Basic_Data_Structures/genric-tree/ lowest-common-ancestor-generic-tree.java
@@ -1,1 +1,100 @@
-// sacascf
+// Lowest Common Ancestor (generic Tree)
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  private static class Node {
+    int data;
+    ArrayList<Node> children = new ArrayList<>();
+  }
+
+  public static void display(Node node) {
+    String str = node.data + " -> ";
+    for (Node child : node.children) {
+      str += child.data + ", ";
+    }
+    str += ".";
+    System.out.println(str);
+
+    for (Node child : node.children) {
+      display(child);
+    }
+  }
+
+  public static Node construct(int[] arr) {
+    Node root = null;
+
+    Stack<Node> st = new Stack<>();
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == -1) {
+        st.pop();
+      } else {
+        Node t = new Node();
+        t.data = arr[i];
+
+        if (st.size() > 0) {
+          st.peek().children.add(t);
+        } else {
+          root = t;
+        }
+
+        st.push(t);
+      }
+    }
+
+    return root;
+  }
+
+  public static ArrayList<Integer> nodeToRootPath(Node node, int data) {
+    if (node.data == data) {
+      ArrayList<Integer> path = new ArrayList<>();
+      path.add(node.data);
+      return path;
+    }
+
+    for (Node child : node.children) {
+      ArrayList<Integer> ptc = nodeToRootPath(child, data);
+      if (ptc.size() > 0) {
+        ptc.add(node.data);
+        return ptc;
+      }
+    }
+
+    return new ArrayList<>();
+  }
+
+  public static int lca(Node node, int d1, int d2) {
+    ArrayList<Integer> ntr1 = nodeToRootPath(node, d1);
+    ArrayList<Integer> ntr2 = nodeToRootPath(node, d2);
+    int size1 = ntr1.size() - 1, size2 = ntr2.size() - 1;
+    while(size1 >= 0 && size2 >= 0){
+        if(ntr1.get(size1) == ntr2.get(size2)){
+            size1--;
+            size2--;
+        }
+        else break;
+    }
+    return ntr1.get(size1+1);
+    
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(values[i]);
+    }
+
+    int d1 = Integer.parseInt(br.readLine());
+    int d2 = Integer.parseInt(br.readLine());
+
+    Node root = construct(arr);
+    int lca = lca(root, d1, d2);
+    System.out.println(lca);
+    // display(root);
+  }
+
+}
