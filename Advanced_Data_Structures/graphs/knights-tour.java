14807d473a55195fb074f1f1321b68dab9d873cc
pidwid <luckyshukla765@gmail.com>
2021-08-30 00:48:58 +0530

    graph

---
+++ Advanced_Data_Structures/graphs/knights-tour.java
@@ -0,0 +1,45 @@
+// Knights Tour
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner  sc = new Scanner(System.in);
+        int n = sc.nextInt(), row = sc.nextInt(), col = sc.nextInt();
+        int[][] chess = new int[n][n];
+        printKnightsTour(chess, row, col, 1);
+    }
+
+    public static void printKnightsTour(int[][] chess, int r, int c, int upcomingMove) {
+        if(r >= chess.length || r < 0 || c < 0 || c >= chess.length || chess[r][c] > 0) return;
+        if(upcomingMove == chess.length * chess.length){
+            chess[r][c] = upcomingMove;
+            displayBoard(chess);
+            chess[r][c] = 0;
+            return;
+        }
+        chess[r][c] = upcomingMove;
+        printKnightsTour(chess, r - 2, c + 1, upcomingMove + 1);
+        printKnightsTour(chess, r - 1, c + 2, upcomingMove + 1);
+        printKnightsTour(chess, r + 1, c + 2, upcomingMove + 1);
+        printKnightsTour(chess, r + 2, c + 1, upcomingMove + 1);
+        printKnightsTour(chess, r + 2, c - 1, upcomingMove + 1);
+        printKnightsTour(chess, r + 1, c - 2, upcomingMove + 1);
+        printKnightsTour(chess, r - 1, c - 2, upcomingMove + 1);
+        printKnightsTour(chess, r - 2, c - 1, upcomingMove + 1);
+        chess[r][c] = 0;
+    }
+
+    public static void displayBoard(int[][] chess){
+        for(int i = 0; i < chess.length; i++){
+            for(int j = 0; j < chess[0].length; j++){
+                System.out.print(chess[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+    }
+}
